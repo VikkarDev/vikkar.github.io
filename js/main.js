5949dfc5784548b55724d0819608ff3b127cbd1d
Vikkar <viktorka2014@gmail.com>
2022-09-02 20:06:50 +0200

    Website update

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const about = document.querySelector('#about')
+const contact = document.querySelector('#contact')
+const aboutContent = document.querySelector('#about-content')
+const contactContent = document.querySelector('#contact-content')
+
+about.addEventListener('click', () => {
+    const aboutBox = new WinBox({
+        title: 'About Me',
+        background: '#00aa00',
+        width: '400px',
+        height: '400px',
+        top: 50, 
+        right: 50, 
+        bottom: 50, 
+        left: 50,
+        mount: aboutContent
+    })
+})
+
+contact.addEventListener('click', () => {
+    const contactBox = new WinBox({
+        title: 'Contact',
+        background: '#00aa00',
+        width: '400px',
+        height: '400px',
+        top: 150, 
+        right: 50, 
+        bottom: 50, 
+        left: 150,
+        mount: contactContent
+    })
+})
